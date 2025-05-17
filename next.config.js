602fe75d8082f29dcc5accd43f00f48837fc826a
steveleecode <125843025+steveleecode@users.noreply.github.com>
2025-05-16 19:15:44 -0700

    Added reroute for sitemap in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/sitemap.xml/', // ← add trailing slash so App Router picks it up
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig; 
